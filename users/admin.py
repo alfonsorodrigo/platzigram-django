5f994163a67d01e855df8ee35156668743453f47
Alfonso Feria <alfonso.feria@konfio.mx>
2020-10-11 23:57:58 -0500

    The panel administrator is customized

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from users.models import Profile
-# Register your models here.
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("pk", "user", "phone_number", "website", "picture")
+    list_display_links = ("pk", "user")
+    list_editable = ("phone_number", "website", "picture")
+    search_fields = (
+        "user__email",
+        "user__username",
+        "user__first_name",
+        "user__last_name",
+        "phone_number",
+    )
+    list_filter = ("created", "updated", "user__is_active", "user__is_staff")
