66e94229d2004fcd3fc71e546e8f1c83dd8024d8
Alfonso Feria <alfonso.feria@konfio.mx>
2020-10-16 17:55:13 -0500

    Added Logout View

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def login_view(request):
@@ -14,3 +15,10 @@ def login_view(request):
         else:
             return render(request,'users/login.html',{'error':'Invalid username and password'})
     return render(request,'users/login.html')
+
+
+@login_required
+def logout_view(request):
+    """Logout a user."""
+    logout(request)
+    return redirect('login')
